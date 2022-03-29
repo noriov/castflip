d8a15c242009312c5362c0e998166731241a977d
Norio Goda <noriov@tutanota.com>
2022-03-29 19:02:25 +0900

    Fix editorial mistakes. in castflip_test/tests/api/u2_mem_*.rs

--- castflip_test/tests/api/u2_mem_decastv.rs
+++ castflip_test/tests/api/u2_mem_decastv.rs
@@ -8,12 +8,12 @@ use crate::{UData2, NELEM2};
 fn udata2() {
     let udata2 = UData2::gen();
 
-    let mut ne_bytes_from_ne = [0_u8; size_of::<u32>() * NELEM2];
+    let mut ne_bytes = [0_u8; size_of::<u32>() * NELEM2];
 
     let ne_vec = udata2.ne_vals.array.to_vec();
 
-    let ne_size = ne_bytes_from_ne.decastv::<u32>(&ne_vec).unwrap();
+    let ne_size = ne_bytes.decastv::<u32>(&ne_vec).unwrap();
 
     assert_eq!(ne_size, size_of::<u32>() * NELEM2);
-    assert_eq!(ne_bytes_from_ne, udata2.ne_bytes);
+    assert_eq!(ne_bytes, udata2.ne_bytes);
 }
--- castflip_test/tests/api/u2_mem_encasts.rs
+++ castflip_test/tests/api/u2_mem_encasts.rs
@@ -8,12 +8,12 @@ use crate::{UData2, NELEM2};
 fn udata2() {
     let udata2 = UData2::gen();
 
-    let mut ne_array_from_raw = [0_u32; NELEM2];
+    let mut ne_array_from_ne = [0_u32; NELEM2];
 
-    let ne_slice = &mut ne_array_from_raw[..];
+    let ne_slice = &mut ne_array_from_ne[..];
 
     let ne_size = udata2.ne_bytes.encasts::<u32>(ne_slice).unwrap();
 
     assert_eq!(ne_size, size_of::<u32>() * NELEM2);
-    assert_eq!(ne_array_from_raw, udata2.ne_vals.array);
+    assert_eq!(ne_array_from_ne, udata2.ne_vals.array);
 }
